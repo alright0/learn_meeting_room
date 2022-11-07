a1bd77c9639e2fe7b384e5590597f7200705f8de
Mikhail Postnikov <postnikovmu@mail.ru>
2022-11-07 13:48:20 +0300

    drf_yasg/swagger is added

--- meeting_room/meeting_room/urls.py
+++ meeting_room/meeting_room/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        #  add your swagger doc title
+        title="Showroom API",
+        #  version of the swagger doc
+        default_version='v1',
+        # first line that appears on the top of the doc
+        description="Test description",
+    ),
+    public=True,
+)
 
 urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),    
     path('admin/', admin.site.urls),
     re_path(r'^booking_system/', include('booking_system.urls')),
 ]
