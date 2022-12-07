d45006991c46690d518fd25b8fc14445102a77ac
Mikhail Postnikov <postnikovmu@mail.ru>
2022-12-07 20:06:23 +0300

    Room list view and template are created

--- meeting_room/booking_system/urls.py
+++ meeting_room/booking_system/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('t_room_list/', views.t_room_list),
     path('t_booking_list/', views.t_booking_list),
     path('booking_list/', views.booking_list),
     path('t_booking_details/<int:booking_id>/', views.booking_details,
--- meeting_room/booking_system/views.py
+++ meeting_room/booking_system/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Booking
+from .models import Booking, Room
 from .serializers import BookingSerializer
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -31,4 +31,13 @@ def t_booking_list(request):
         bookings = Booking.objects.all()
         data = {"bookings": bookings}
         return render(request, 'booking_system/t_booking_list.html',
+                      context=data)
+
+
+@login_required()
+def t_room_list(request):
+    if request.method == 'GET':
+        rooms = Room.objects.all()
+        data = {"rooms": rooms}
+        return render(request, 'booking_system/t_room_list.html',
                       context=data)
